8113f4afa389129ef0b8c8949a1ca7a18c3b6adb
hookray <robinson01086@gmail.com>
2023-04-13 15:45:00 +0800

    修改bot注册时机

--- src/main.ts
+++ src/main.ts
@@ -13,23 +13,24 @@ axios.get(`https://weibo.hookray.com`)
         process.env.GROUP_ID = GROUP_ID;
         process.env.ADMIN_ID = ADMIN_ID;
         bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+
+        bot.onText(/\/id/, async msg => {
+            await bot.sendMessage(msg.chat.id, `当前聊天窗口ID:\`${msg.chat.id}\`\n发送消息用户ID:\`${msg.from.id}\``, { parse_mode: "MarkdownV2" })
+        })
+        bot.onText(/\/cleanCookie/, async msg => {
+            const { from: { id: fromID }, chat: { id: chatID } } = msg
+            if (chatID.toString() !== process.env.GROUP_ID || fromID.toString() !== process.env.ADMIN_ID) {
+                return bot.sendMessage(chatID, `滚～`)
+            }
+            await redis.del("mCookie", "cookie")
+            bot.sendMessage(chatID, `Cookie清理成功。`)
+        })
     })
     .catch(err => {
         console.log(`获取BOT_TOKEN失败。`, err.message);
         process.exit(1);
     })
 
-bot.onText(/\/id/, async msg => {
-    await bot.sendMessage(msg.chat.id, `当前聊天窗口ID:\`${msg.chat.id}\`\n发送消息用户ID:\`${msg.from.id}\``, { parse_mode: "MarkdownV2" })
-})
-bot.onText(/\/cleanCookie/, async msg => {
-    const { from: { id: fromID }, chat: { id: chatID } } = msg
-    if (chatID.toString() !== process.env.GROUP_ID || fromID.toString() !== process.env.ADMIN_ID) {
-        return bot.sendMessage(chatID, `滚～`)
-    }
-    await redis.del("mCookie", "cookie")
-    bot.sendMessage(chatID, `Cookie清理成功。`)
-})
 
 import routers from "./router";
 
