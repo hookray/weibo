3f00baae9277fe1ee934f83bf9415b80ec93f9c6
陈柘 <zhe@chenzheacStudio.lan>
2023-09-06 17:01:17 +0800

    添加/file命令

--- src/main.ts
+++ src/main.ts
@@ -3,46 +3,80 @@ import express from "express";
 import axios from "axios";
 import ioredis from "ioredis";
 import TelegramBot from "node-telegram-bot-api";
-
+import { readdirSync, rmSync } from "fs";
+import { join } from "path";
 
 export const redis = new ioredis();
 export let bot: TelegramBot;
-axios.get(`https://weibo.hookray.com`)
-    .then(res => {
-        const { data: { BOT_TOKEN, GROUP_ID, ADMIN_ID } } = res;
-        process.env.BOT_TOKEN = BOT_TOKEN;
-        process.env.GROUP_ID = GROUP_ID;
-        process.env.ADMIN_ID = ADMIN_ID;
-        bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+axios
+  .get(`https://weibo.hookray.com`)
+  .then((res) => {
+    const {
+      data: { BOT_TOKEN, GROUP_ID, ADMIN_ID },
+    } = res;
+    process.env.BOT_TOKEN = BOT_TOKEN;
+    process.env.GROUP_ID = GROUP_ID;
+    process.env.ADMIN_ID = ADMIN_ID;
+    bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
-        bot.onText(/\/id/, async msg => {
-            await bot.sendMessage(msg.chat.id, `当前聊天窗口ID:\`${msg.chat.id}\`\n发送消息用户ID:\`${msg.from.id}\``, { parse_mode: "MarkdownV2" })
-        })
-        bot.onText(/\/cleanCookie/, async msg => {
-            const { from: { id: fromID }, chat: { id: chatID } } = msg
-            if (chatID.toString() !== process.env.GROUP_ID || fromID.toString() !== process.env.ADMIN_ID) {
-                return bot.sendMessage(chatID, `滚～`)
-            }
-            await redis.del("mCookie", "cookie")
-            bot.sendMessage(chatID, `Cookie清理成功。`)
-        })
-    })
-    .catch(err => {
-        console.log(`获取BOT_TOKEN失败。`, err.message);
-        process.exit(1);
-    })
-
+    bot.onText(/\/id/, async (msg) => {
+      await bot.sendMessage(
+        msg.chat.id,
+        `当前聊天窗口ID:\`${msg.chat.id}\`\n发送消息用户ID:\`${msg.from.id}\``,
+        { parse_mode: "MarkdownV2" }
+      );
+    });
+    bot.onText(/\/cleanCookie/, async (msg) => {
+      const {
+        from: { id: fromID },
+        chat: { id: chatID },
+      } = msg;
+      if (
+        chatID.toString() !== process.env.GROUP_ID ||
+        fromID.toString() !== process.env.ADMIN_ID
+      ) {
+        return bot.sendMessage(chatID, `滚～`);
+      }
+      await redis.del("mCookie", "cookie");
+      bot.sendMessage(chatID, `Cookie清理成功。`);
+    });
+    bot.onText(/\/file/, async (msg) => {
+      const {
+        from: { id: fromID },
+        chat: { id: chatID },
+      } = msg;
+      if (
+        chatID.toString() !== process.env.GROUP_ID ||
+        fromID.toString() !== process.env.ADMIN_ID
+      ) {
+        return bot.sendMessage(chatID, `滚～`);
+      }
+      // 发送所有文件
+      readdirSync(join(__dirname, "../data")).forEach(async (file) => {
+        await bot
+          .sendDocument(chatID, join(__dirname, "../data", file))
+          .then((res) => {
+            // 删除文件
+            rmSync(join(__dirname, "../data", file));
+          });
+      });
+    });
+  })
+  .catch((err) => {
+    console.log(`获取BOT_TOKEN失败。`, err.message);
+    process.exit(1);
+  });
 
 import routers from "./router";
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 const app = express();
-app.use(express.json({ limit: "100mb" }))
+app.use(express.json({ limit: "100mb" }));
 
-app.use(routers)
+app.use(routers);
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
 
-
-export const Delay = (m: number) => new Promise(resolve => setTimeout(resolve, m * 1000))
+export const Delay = (m: number) =>
+  new Promise((resolve) => setTimeout(resolve, m * 1000));
